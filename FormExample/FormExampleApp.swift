5f8ac35055ada40975f4bb7acb0887a24ae13f76
Ahmet Karadağ <akkaradaga@gmail.com>
2024-10-15 14:43:01 +0300

    Initial Commit

---
+++ FormExample/FormExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormExampleApp.swift
+//  FormExample
+//
+//  Created by ahmet karadağ on 15.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct FormExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
